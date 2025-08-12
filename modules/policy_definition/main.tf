765b000f840964e7c1dd3c8567fa3c1eb3637026
まっぴぃ⊿ <46888598+ymasaoka@users.noreply.github.com>
2025-08-12 14:32:45 +0900

    Refactor policy_definition module
    Renamed variable.tf to variables.tf and reordered variables for clarity. Added default values for optional variables and commented out optional arguments in main.tf to improve maintainability. Updated .gitignore to allow tracking of tfvars files in envs subdirectories.

--- modules/policy_definition/main.tf
+++ modules/policy_definition/main.tf
@@ -1,13 +1,14 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/policy_definition
 resource "azurerm_policy_definition" "this" {
   # Required
-  name                = var.name
   policy_type         = var.policy_type
-  mode                = var.mode
+  name                = var.name
   display_name        = var.display_name
+  mode                = var.mode
   # Optional
-  description         = var.description
-  management_group_id = var.management_group_id
-  policy_rule         = var.policy_rule
-  metadata            = var.metadata
-  parameters          = var.parameters
+  # description         = var.description
+  # management_group_id = var.management_group_id
+  # policy_rule         = var.policy_rule
+  # metadata            = var.metadata
+  # parameters          = var.parameters
 }
